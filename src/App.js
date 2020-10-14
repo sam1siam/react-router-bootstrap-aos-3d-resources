319cc470e04879898c70d11694be5d137ba078ea
Samer <66347142+wwidprog@users.noreply.github.com>
2020-10-14 06:44:08 -0400

    Added light mode feature

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Switch, Link } from "react-router-dom"
+import React, {useState} from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom"
 import Home from "./pages/Home.js"
 import DemoPage from "./pages/DemoPage.js"
 import SignUp from "./pages/SignUp.js"
 import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
+import { CgDarkMode } from "react-icons/cg";
 
 
 const App = () => {
+const [darkMode, setDarkMode] = useState(false)
   return (
-    <div className="App">
+    <>
+    <CgDarkMode id="modeToggle" onClick={() => setDarkMode(darkMode => !darkMode)} className="Icon-DarkMode" />
+    <div className={darkMode ? "dark-mode App" : "App" }>
     <Router>
-    <Header />
+    <Header/>
      <Switch>
       <Route path="/demo" component={DemoPage}/>
       <Route path="/signup" component={SignUp}/>
@@ -20,7 +24,10 @@ const App = () => {
     </Router>
     <Footer />
     </div>
+    </>
   );
 };
 
 export default App;
+
+// 
